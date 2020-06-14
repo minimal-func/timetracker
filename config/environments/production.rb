5f7d7f55acc85be8e8f16964734002fe2728fa06
Ildar Safin <safild94@gmail.com>
2020-06-14 16:07:24 +0200

    Add cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -90,4 +90,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+       origins '*'
+       resource '*', :headers => :any, :methods => [:get, :post, :options]
+     end
+  end
 end
