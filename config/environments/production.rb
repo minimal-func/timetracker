c07de1dfedc8316356789095214aa0d5a8708ff5
Ildar Safin <safild94@gmail.com>
2020-06-14 17:58:51 +0200

    Remove hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.hosts << 'tracker.ildarsafin.tech'
-
 end
